3571e09aee94d702d7729a00cd9f4f385eb4df03
Metin Ağaoğlu <metnagaoglu@gmail.com>
2019-10-03 21:08:34 +0300

    New theme implemented using material design

--- index.php
+++ index.php
@@ -2,23 +2,33 @@
 <!DOCTYPE html>
 <html lang="tr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Audio Converter</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title><?= gettext('Audio Converter') ?></title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
 
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">
-        Audio Converter
-    </a>
+<nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Simple Mp3 Converter</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="index.php">Homepage</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav">
+            <li><a href="index.php">Homepage</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
 </nav>
 <br>
 
-<div class="container">
+<div class="section no-pad-bot" id="index-banner">
+    <div class="container">
 
 	<?= getFlashMessageIfExists() ?>
 
@@ -31,42 +41,56 @@
 	<?php endif; ?>
 	<?php flushFlashMessage(); ?>
 
-    <div class="row">
-        <div class="col">
-            <form action="upload.php" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="csrf_token" value="<?= getCsrfToken() ?>">
-                <div class="form-group">
-                    <label><?php gettext('Audio file') ?></label>
-                    <input type="file" class="form-control-file" name="sound_file">
-                </div>
+        <div class="row">
+            <div class="col s6 col s12">
+                <form action="upload.php" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="csrf_token" value="<?= getCsrfToken() ?>">
+                    <div class="file-field input-field">
+                        <div class="btn">
+                            <span><?= gettext('Audio file') ?></span>
+                            <input type="file" name="sound_file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input type="submit" class="btn btn-primary" name="convert_and_download">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col s6 col s12">
+                <h2><?= gettext('History') ?></h2>
+                <?php foreach (getUploadedMp3ByUserId() as $value) {
+                    ?>
+                        <div class="col s12 m6">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title"></span>
+                                    <audio controls style="width:100%;">
+                                        <source src="stream.php?file=<?= $value ?>" type="audio/mpeg">
+										<?= gettext('Your browser does not support the audio element.') ?>
+                                    </audio>
+                                </div>
+                                <div class="card-action">
+                                    <a href="stream.php?file=<?= $value ?>"><?= gettext('Download') ?></a>
+                                </div>
+                            </div>
+                        </div>
 
-                <input type="submit" class="btn btn-primary" name="convert_and_download">
-            </form>
+                    <?php
+                } ?>
+            </div>
         </div>
-        <div class="col">
-            <h2><?= gettext('History') ?></h2>
-			<?php foreach (getUploadedMp3ByUserId() as $value) {
-				?>
-                <audio controls>
-                    <source src="stream.php?file=<?= $value ?>" type="audio/mpeg">
-                    <?= gettext('Your browser does not support the audio element.') ?>
-                </audio>
-				<?php
-			} ?>
-        </div>
+
     </div>
-
-
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="assets/js/materialize.min.js"></script>
+    <script src="assets/js/init.js"></script>
 </body>
 </html>
